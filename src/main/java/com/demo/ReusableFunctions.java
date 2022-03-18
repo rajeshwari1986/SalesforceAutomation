c862d835bd56d658d71b69327df9148ae37f3812
mithun-reddy <mithun.r@tekarch.com>
2022-03-18 11:08:01 -0700

    Created reusable functions

---
+++ src/main/java/com/demo/ReusableFunctions.java
@@ -0,0 +1,112 @@
+package com.demo;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * @author user
+ *
+ */
+public class ReusableFunctions {
+
+	public static void enterText(WebElement element, String text) {
+		element.sendKeys(text);
+	}
+
+	/**
+	 * This function will select an option from user menu drop down
+	 * 
+	 * @param driver     Webdriver
+	 * @param optionName "My Profile", "Logout", "", ""
+	 * @return {boolean} true if option is selected else false
+	 */
+	public static boolean selectOptionInUserMenu(WebDriver driver, String optionName) {
+		boolean isOptionSelected = false;
+		WebElement userMenuOption = driver.findElement(By.xpath("//a[text()='" + optionName+ "']"));
+		if (userMenuOption.isDisplayed()) {
+			userMenuOption.click();
+			isOptionSelected = true;
+		} else {
+			System.out.println("Usermenu option " + optionName + " is not visible");
+		}
+		return isOptionSelected;
+	}
+
+	public static boolean verifyUserMenuItems(WebDriver driver) {
+		boolean isOptionVerified = true;
+		String[] userMenuOptions = { "My Profile", "My Settings", "Developer Console", "Switch to Lightning Experience",
+				"Logout" };
+		List<WebElement> listOfuserMenuOptions = driver.findElements(By.xpath("(//div[@id='userNav-menuItems']//a)"));
+		for (int i = 0; i < userMenuOptions.length; i++) {
+			String optionValue = listOfuserMenuOptions.get(i).getText();
+			if (optionValue.equals(userMenuOptions[i])) {
+				System.out.println("Option " + userMenuOptions[i] + " is verified");
+			} else {
+				System.out.println("Verification of " + userMenuOptions[i] + " failed");
+				isOptionVerified = false;
+			}
+		}
+		return isOptionVerified;
+	}
+
+//	public boolean clickOnProfile(WebElement profileElement) {
+//		if (profileElement.isDisplayed()) {
+//			profileElement.click();
+//			return true;
+//		} else
+//			return false;
+//	}
+
+	public static boolean clickOnElement(WebElement element) {
+		if (element.isDisplayed()) {
+			element.click();
+			return true;
+		} else
+			return false;
+	}
+
+//	public boolean clickOnPostLink(WebElement postLink) {
+//		if (postLink.isDisplayed()) {
+//			postLink.click();
+//			return true;
+//		} else
+//			return false;
+//	}
+
+	public static boolean createAPost(WebElement postTextBox, WebElement shareButton, String message) {
+		boolean isPostCreated = false;
+		if (postTextBox.isDisplayed()) {
+			postTextBox.sendKeys(message);
+			if (shareButton.isEnabled()) {
+				shareButton.click();
+				System.out.println("Created a post successfully");
+				isPostCreated = true;
+			} else {
+				System.out.println("Failed to click on the share button");
+			}
+		}
+		return isPostCreated;
+	}
+	
+	public static void clickOnFileUpload(WebElement fileUplaod) {
+		fileUplaod.click();
+	}
+	
+	public static void uploadFile(WebElement uploadFile, WebElement shareButton, String filePath) {
+		if(uploadFile.isDisplayed()) {
+			uploadFile.sendKeys(filePath);
+			shareButton.click();
+		}
+	}
+	
+	public static void selectOptionFromDropdown(WebElement element, int index) {
+		Select options = new Select(element);
+		options.selectByIndex(index);
+		
+	}
+
+}
--- src/main/java/com/demo/SeleniumSession.java
+++ src/main/java/com/demo/SeleniumSession.java
@@ -19,20 +19,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumSession {
 	static WebDriver driver;
-	
+
 	static {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 	}
-	
+
 	public static void loginError01() {
 		driver.get("https://login.salesforce.com");
 		driver.navigate().to("https://google.com");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.findElement(By.name("username")).sendKeys("lakshmip@sales.com");
+		WebElement username = driver.findElement(By.name("username"));
+		ReusableFunctions.enterText(username, "lakshmip@sales.com");
 		driver.findElement(By.id("password")).sendKeys("Lakshmi123");
-		driver.findElement(By.name("Login")).click();
+		WebElement loginButton = driver.findElement(By.name("Login"));
+		ReusableFunctions.clickOnElement(loginButton);
 		String actualTitle = "Home Page ~ Salesforce - Developer Edition";
 		if (actualTitle.equals(driver.getTitle())) {
 			System.out.println("My TC 1 is passed");
@@ -40,27 +42,39 @@ public class SeleniumSession {
 			System.out.println("My TC1 failed");
 	}
 
+	public static void checkRememberMe() throws InterruptedException {
+		driver.get("https://login.salesforce.com");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.findElement(By.name("username")).sendKeys("lakshmip@sales.com");
+		driver.findElement(By.id("password")).sendKeys("Lakshmi123");
+		driver.findElement(By.name("rememberUn")).click();
+		driver.findElement(By.name("Login")).click();
+		Thread.sleep(10000);
+		if(driver.findElement(By.id("userNavLabel")).isDisplayed()) {
+			driver.findElement(By.id("userNavLabel")).click();
+		}
+		else {
+			System.out.println("Usermenu is not visible");
+		}
+		driver.findElement(By.xpath("//a[@title='Logout']")).click();
+		Thread.sleep(6000);
+		// C:\Users\user\Desktop\Collectionframework.png
+		driver.findElement(By.xpath("//*[@id=\"j_id0:uploadFileForm:uploadInputFile\"]")).sendKeys("C:\\Users\\user\\Desktop\\Collectionframework.png");
+	}
+
 	public static void main(String[] args) throws InterruptedException {
-		
-	
-		
-		String s = "mithun";
-		WebElement username = driver.findElement(By.id("username"));
-		username.sendKeys("nsjdbfusydf");
-		username.click();
-		
-		List<WebElement> ribbonElements = driver.findElements(By.xpath("//li"));
-		
-		
+		checkRememberMe();
+//		String s = "mithun";
+//		WebElement username = driver.findElement(By.id("username"));
+//		username.sendKeys("nsjdbfusydf");
+//		username.click();
+//		driver.findElement(By.cssSelector("a[title='Home Tab']")).click();
+//		List<WebElement> ribbonElements = driver.findElements(By.xpath("//li"));
 		// Explicit wait
-		
 //		WebDriverWait wait = new WebDriverWait(driver, 30);
 //		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(""))));
-		
-		
-		
 		// Fluent wait
-		
 //		Wait<WebDriver> wait1 = new FluentWait<WebDriver>(driver)
 //				.withTimeout(Duration.ofSeconds(30))
 //				.pollingEvery(Duration.ofSeconds(10))
@@ -71,8 +85,8 @@ public class SeleniumSession {
 //				return driver.findElement(By.id("Login"));
 //			}
 //		});
-		
-		driver.quit();
+
+//		driver.quit();
 		driver.close();
 	}
 
