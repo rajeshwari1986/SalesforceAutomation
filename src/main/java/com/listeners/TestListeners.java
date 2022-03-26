76cd18b20349e076d2b446ed4dbe8fec9b22b6ac
mithun-reddy <mithun.r@tekarch.com>
2022-03-26 09:14:25 -0700

    Listeners

---
+++ src/main/java/com/listeners/TestListeners.java
@@ -0,0 +1,67 @@
+package com.listeners;
+
+import java.io.IOException;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentTest;
+
+import sfdc.test.BaseTest;
+import sfdc.utilities.ReusableUtils;
+
+public class TestListeners implements ITestListener {
+	
+	ReusableUtils ru = new ReusableUtils();
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		BaseTest.test = BaseTest.extent.createTest(result.getName());
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test script passed ");
+		
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test script Failed ");
+		try {
+			BaseTest.test.addScreenCaptureFromPath(ru.takeScreenshot());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		BaseTest.test.fail(result.getName());
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
